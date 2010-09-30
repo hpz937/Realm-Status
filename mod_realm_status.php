cba4c5a2abbe22ed7e8739689b7635886c7185c0
hpz937 <hpz937@gmail.com>
2010-09-30 14:32:44 -0500

    fix template php

--- mod_realm_status.php
+++ mod_realm_status.php
@@ -3,7 +3,7 @@
 defined('_JEXEC') or die('Direct Access to this location is not allowed.');
 require_once( dirname(__FILE__).DS.'helper.php' );
 
-modRealmStatus::wow_ss($params->get('realm'),$params->get('display'),$params->get('region'),$params->get('updatetimer'),'modules/mod_realm_status/images/',$params->get('imagetype'));
+$data = modRealmStatus::wow_ss($params->get('realm'),$params->get('display'),$params->get('region'),$params->get('updatetimer'),'modules/mod_realm_status/images/',$params->get('imagetype'));
 require( JModuleHelper::getLayoutPath( 'mod_realm_status' ) );
 
 ?>
--- templ/default.php
+++ templ/default.php
@@ -1,1 +1,3 @@
 <?php
+echo '<img alt="WoW Server Status for '. $data->realm .'" src="' . $data->imageurl . '" />';
+?>
